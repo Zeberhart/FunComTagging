8aea73a5318ab2180bdf21a5d4986141d27b444c
Zack Eberhart <zacko@Zacks-MacBook-Pro.local>
2018-05-23 00:16:26 -0400

    Simplified migrations

--- db/migrate/20180518145626_create_users.rb
+++ db/migrate/20180518145626_create_users.rb
@@ -3,8 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.text :email
       t.string :password_digest
-      t.text :current_comment_id
-      t.integer :seed
+      t.integer :current_comment_id
       t.timestamps
     end
   end
--- db/migrate/20180521200430_change_current_type_and_remove_seed.rb
+++
@@ -1,6 +0,0 @@
-class ChangeCurrentTypeAndRemoveSeed < ActiveRecord::Migration[5.1]
-  def change
-    change_column :users, :current_comment_id, :integer
-    remove_column :users, :seed
-  end
-end
